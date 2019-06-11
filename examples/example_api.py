51371e12780abe607c4a7eec0c49fea556995c72
dani <daniel.soler@e-campus.uab.cat>
2019-06-11 12:57:47 +0200

    fix minor thing on api example

--- examples/example_api.py
+++ examples/example_api.py
@@ -1,3 +1,4 @@
+# Retrieve Dataset
 
 
 # Analyze dataset
@@ -8,10 +9,11 @@ X, y = datasets.make_blobs(n_samples=1500, random_state=170)
 analysis_cluster = ks.KmeansAlg(X, nclust=3)
 analysis_cluster.analyze()
 
-#Cluster
+# Cluster
 from cluster_drug_discovery.methods import kmeans
 import matplotlib.pyplot as plt
 cluster = kmeans.KmeansAlg(X, nclust=3)
 y_pred = cluster.run()
 plt.plot(X[:, 0], X[:, 1], y_pred)
+
 
