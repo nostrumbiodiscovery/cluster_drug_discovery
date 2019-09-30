e7088f4683884bc7ff7f55fef0b4f38294f0564a
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-09-30 15:30:35 +0200

    include test coverage

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(name='cluster_drug_discovery',
+      packages=find_packages(),
+      version='1.0.0.0',
+      license='MIT',
+      description='Clustering methodologies over pdb trajectories',
+      author='Daniel Soler',
+      author_email='daniel.soler@nostrumbiodiscovery.com',
+      url='https://github.com/NostrumBioDiscovery/cluster_drug_discovery',
+     )
